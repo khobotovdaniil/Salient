0c706616d07186bdca1fa26891e1cca817f876b2
khobotovdaniil <dinich44@gmail.com>
2022-09-15 22:58:38 +0300

    add menu animation

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const navMenu = document.querySelector('nav');
+
+    window.onscroll = function() {
+        if (window.pageYOffset>120) {
+            navMenu.classList.add('nav_dynamic');
+            navMenu.classList.remove('nav_static');
+        } else {
+            navMenu.classList.add('nav_static'); 
+            navMenu.classList.remove('nav_dynamic');
+        }
+    };
+    
+});
