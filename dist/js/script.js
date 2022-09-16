a8386859dd55b244b8f324be8341d4543134136a
khobotovdaniil <dinich44@gmail.com>
2022-09-16 17:25:56 +0300

    add team section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    //static/dynamic menu
     const navMenu = document.querySelector('nav');
 
     window.onscroll = function() {
@@ -10,7 +12,40 @@ window.addEventListener('DOMContentLoaded', () => {
             navMenu.classList.remove('nav_dynamic');
         }
     };
-    
+
+    //highlight menu objects
+    const getId = (link) => link.getAttribute('href').replace('#', '');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                document.querySelectorAll('.nav__navbar__item__link').forEach((link) => {
+                    link.classList.toggle('nav__navbar__item__link_active',
+                    getId(link) === entry.target.id
+                    );
+                });
+            }
+        });
+    }, {
+        threshold: 0.4,
+    });
+
+    document.querySelectorAll('section').forEach((section) => {
+        observer.observe(section);
+    });
+
+    //smooth scroll on click
+    document.querySelector('.nav__navbar').addEventListener('click', (event) => {
+        if (event.target.classList.contains('nav__navbar__item__link')) {
+            event.preventDefault();
+
+            window.scrollTo({
+                top: document.getElementById(getId(event.target)).offsetTop,
+                behavior: 'smooth',
+            });
+        }
+    });
+
 
     new WOW().init();
 });
